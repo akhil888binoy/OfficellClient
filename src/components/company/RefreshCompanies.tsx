473edd4e92e7b48560fd3b2d635eba414167d52e
akhilbinoy <akhilbinoy@akhilbinoys-MacBook-Air.local>
2025-11-29 09:52:58 +0530

    fix:refresh

--- src/components/company/RefreshCompanies.tsx
+++ src/components/company/RefreshCompanies.tsx
@@ -14,8 +14,6 @@ const RefreshCompanies = () => {
 
         const fetchDbCompanies = async()=>{
             try {
-           
-    
             const lastCreatedAt = companies.sort((a,b)=> a.createdAt > b.createdAt ? -1: 1)[0];
             const {data: response } = await axios.get(`${import.meta.env.VITE_API}/companies/count?lastcreatedAt=${lastCreatedAt.createdAt}`,{
                 withCredentials: true
@@ -42,29 +40,40 @@ const RefreshCompanies = () => {
     },[]);
     
     return (
-        <>
-            { refreshButton && pathname ==="/companies" &&
-                <button
-                onClick={handleRefresh}
-                className=" w-55 border border-white text-white px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-white active:bg-white active:text-black hover:text-black transition duration-200 whitespace-nowrap"
-                >
-                    New Companies
-                </button>
-            }
-        {
-            refreshButton && pathname ==="/companies" && 
-            <button
-                onClick={handleRefresh}
-                className={`landscape:hidden items-center justify-center ms-3 mt-2 p-2 
-                hover:bg-gray-800 active:scale-95 transition text-sm sm:hidden
-                focus:outline-none focus:ring-2 focus:ring-gray-200 `}
-                    >
-                    <IoRefreshCircleOutline className="text-white w-full h-6 "/>
-            </button>
-        }
-    
-            </>
-        )
+  <>
+    {refreshButton && pathname === "/companies" && (
+      <button
+        onClick={handleRefresh}
+        className="
+          hidden sm:block
+          fixed bottom-6 left-72
+          border border-white text-white px-12 py-4 
+          rounded-full tracking-widest uppercase font-bold 
+          bg-transparent hover:bg-white active:bg-white 
+          active:text-black hover:text-black transition duration-200 
+          whitespace-nowrap z-50
+        "
+      >
+        New Companies
+      </button>
+    )}
+
+    {refreshButton && pathname === "/companies" && (
+      <button
+        onClick={handleRefresh}
+        className="
+          sm:hidden
+          fixed bottom-6 left-6 
+          p-3 rounded-full bg-gray-900 
+          hover:bg-gray-800 active:scale-95 transition z-50
+        "
+      >
+        <IoRefreshCircleOutline className="text-white h-7 w-7" />
+      </button>
+    )}
+  </>
+);
+
 }
 
 export default RefreshCompanies
